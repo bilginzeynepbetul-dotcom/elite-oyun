298685f9770cb0706be058a8c333bc06c72d3f57
bilginzeynepbetul-dotcom <bilginzeynepbetul@gmail.com>
2026-08-06 17:13:51 +0300

    Add files via upload

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -26,6 +26,9 @@ const files = [
   "007_national_manager_applications.sql",
   "008_password_reset.sql",
   "009_national_tactics.sql",
+  "010_player_contracts.sql",
+  "011_national_u21.sql",
+  "012_season_awards.sql",
   "013_game_settings.sql",
   "014_calendar_friendlies.sql",
 ];
