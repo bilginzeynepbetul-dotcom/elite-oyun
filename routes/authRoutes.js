5dc804a9d83f8f5596335ffc8286903448d4fa69
bilginzeynepbetul-dotcom <bilginzeynepbetul@gmail.com>
2026-08-07 20:47:26 +0300

    Rename bcrypt import to bcryptjs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@
 // ============================================================
 
 const express = require("express");
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { query, withTransaction } = require("./db");
 const clubsRepo = require("./repos/clubsRepo");
